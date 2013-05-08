df9a5797ce03ec05f866397623a22c28e85c0610
Saul St. John <saul@betafive.net>
2013-05-08 16:28:03 -0500

    cleanup, update to v1.99

--- content_script.js
+++ content_script.js
@@ -12,7 +12,7 @@ function each_love(state) {
 	love_list.each( function(index, tmp) {
 		var author = $(this).find("a.planlove").text();
 
-		state.author_set(author);
+		state.author_set(author, this);
 
 		$(this).find("ul > li").each(function(index, tmp) {
 			state.love_process(this);
@@ -23,7 +23,7 @@ function each_love(state) {
 
 function love_collector() {
 	var state = 
-		{author_set: function(a) {
+		{author_set: function(a, e) {
 			this.author = a;
 			this.current_love[a] = [];
 				     },
@@ -42,7 +42,7 @@ message["love"] = each_love(love_collector()).current_love;
 message["type"] = "love-diff";
 
 function expandomatic(author) {
-	var state = 	{author_set: function(a) {
+	var state = 	{author_set: function(a, e) {
 				if (author == a) {
 					this.love_process = this.this_is_it;
 					var btn = $("#expand_" + a);
@@ -55,7 +55,8 @@ function expandomatic(author) {
 					this.love_process = this.this_is_not_it;
 						   },
 			this_is_it: function(l) {
-				$(l).slideToggle();
+				if (!$(l).data("is_new"))
+					$(l).slideToggle();
 				                },
 			this_is_not_it: function(l) {}};
 	return function() { each_love(state); };
@@ -63,40 +64,41 @@ function expandomatic(author) {
 
 function display_cleaner(new_love) {
 	var state = 
-		{author_set: function(a) {
+		{author_set: function(a, e) {
 			this.author = a;
-			this.collapsable = false;
+
+			var expander = $("<button>+</button>");
+			expander.attr("class", "submitinput");
+			expander.attr("id", "expand_" + this.author);
+			expander.css("float", "none");
+			expander.css("margin-left", "0.5em");
+			expander.click(expandomatic(this.author));
+			
+			var new_div = $("<div/>");
+			
+			var detached = $(e).find("div > a.planlove").detach();
+			new_div.append(detached);
+			
+			detached = $(e).find("div > span").detach();
+			detached.css("margin-left", "0.4em");
+			new_div.append(detached);
+
+			new_div.append(expander);
+
+			$(e).find("div > ul").before(new_div);
 					 },
 		 love_process: function(l) {
 			var txt = $(l).children("span").text();
 
-			var is_new = false;
+			$(l).data("is_new", false);
 			if (this.author in new_love) {
 				for (item in new_love[this.author]) {
 					if (txt == new_love[this.author][item])
-						is_new = true;
+						$(l).data("is_new", true);
 				}
 			}
 		
-			if (!is_new) {
-				if (!this.collapsable) {
-					var expander = $("<button>+</button>");
-					expander.attr("class", "submitinput");
-					expander.attr("id", "expand_" + this.author);
-					expander.css("float", "none");
-					expander.css("margin-left", "0.5em");
-					expander.click(expandomatic(this.author));
-					var par = $(l).parent().parent();
-					var detached = par.children("a").detach();
-					var new_div = $("<div/>");
-					new_div.append(detached);
-					detached = par.children("span").detach();
-					detached.css("margin-left", "0.4em");
-					new_div.append(detached);
-					new_div.append(expander);
-					par.children("ul").before(new_div);
-					this.collapsable = true;
-				}
+			if (!$(l).data("is_new")) {
 				$(l).hide();
 			}
 					 }
--- upgrade.js
+++ upgrade.js
@@ -7,40 +7,8 @@ function minorUpgrade(from) {
 function revUpgrade(from) {
 }
 
-function welcome(maj, min, rev) {
-	var ver = '' + maj + '.' + min + '.' + rev;
-	var notice = "v";
-        notice += ver;
-	notice += " has been installed";
-	var notification = webkitNotifications.createNotification(
-		'alert.png',
-		'Nouvel Amour',
-		notice);
-
-	notification.show();
-	setTimeout(function() {
-		                notification.close();
-			      }, 10000);
-}
-
-function upgraded(maj, min, rev) {
-	var ver = '' + maj + '.' + min + '.' + rev;
-	var notice = "has been updated to version ";
-	notice += ver + ".";
-	
-	var notification = webkitNotifications.createNotification(
-		'alert.png',
-		'Nouvel Amour',
-		notice);
-	
-	notification.show();
-	setTimeout(function() {
-		                notification.close();
-			      }, 10000);
-} 
-
 chrome.runtime.onInstalled.addListener(function(details) {
-	if (details.reason == "chrome_update")
+	if (details.reason != "upgrade")
 		return;
 
 	var curVersion = chrome.app.getDetails().version.split(".");
@@ -48,28 +16,19 @@ chrome.runtime.onInstalled.addListener(function(details) {
 	var cmin = curVersion[1];
 	var crev = curVersion[2];
 	
-	if (details.reason == "install") {
-		welcome(cmaj, cmin, crev);
-		return;
-	} 
-
 	var oldVersion = details.previousVersion.split(".");
 	var omaj = oldVersion[0];
 	var omin = oldVersion[1];
 	var orev = oldVersion[2];
 	
-	if (omaj != cmaj || omin != cmin)
-	{
-		if (omaj < cmaj) {
-			majorUpgrade(omaj);
-			omin = 0;
-			orev = 0;
-		}
-		if (omin < cmin) {
-			minorUpgrade(omin);
-			orev = 0;
-		}
-		upgraded(cmaj, cmin, crev);
+	if (omaj < cmaj) {
+		majorUpgrade(omaj);
+		omin = 0;
+		orev = 0;
+	}
+	if (omin < cmin) {
+		minorUpgrade(omin);
+		orev = 0;
 	}
 	if (orev < crev)
 		revUpgrade(orev);
