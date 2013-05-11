a33b7a78200c0acec77229bd3e90d92aa1f0283f
Saul St. John <saul@betafive.net>
2013-05-11 05:18:45 -0500

    desktop notifications, pt 2 (less broken, not working)

--- notification.js
+++ notification.js
@@ -1,19 +1,27 @@
 function poll() {
-	var resp = $.get("http://grinnellplans.com/quicklove.php");
-	var ql_doc = $(resp.responseText);
-	var love_list = ql_doc("#search_results > li");
+	$.ajax({"url": "http://grinnellplans.com/quicklove.php",
+		"dataType": "html",
+		"success": function(data, text_status, jq) {
+			var $doc = $(data);
+			var user = $doc.find("#mysearch").attr("value");
+			var love_list = $doc.find("#search_results > li");
 
-	var user = ql_doc("#mysearch").val();
+			/*
+			love_list.each(function(index, tmp) {
+				var author = $(this).find("a.planlove").text();
+				msg["love"][author] = [];
 
-	love_list.each(function(index, tmp) {
-		var author = $(this).find("a.planlove").text();
-		msg["love"][author] = [];
+				$(this).find("ul > li").each(function(index, tmp) {
+					msg["love"][author].push($(this).children("span").text());
+				});
+			});
+			*/
 
-		$(this).find("ul > li").each(function(index, tmp) {
-			msg["love"][author].push($(this).children("span").text());
-		});
-	});
-
+			var af1 = $doc.find(".autoreadlevel.first .planlove");
+			var af2 = $doc.find(".autoreadlevel.odd .planlove");
+			var af3 = $doc.find(".autoreadlevel.last .planlove");
+		}});
+}
 
 
 setTimeout(function() {
--- options.js
+++ options.js
@@ -11,9 +11,39 @@ function autodismiss_set() {
 	localStorage["autodismiss"] = $("#autodismiss option:selected").val();
 }
 
+function notification_toggle() {
+	$("#notification_settings").slideToggle();
+	if ("true" == localStorage["notify"] || false)
+		localStorage["notify"] = "false";
+	else
+		localStorage["notify"] = "true";
+}
+
 $(document).ready(function() {
 	$("#clear").click(clear_storage);
 	if ("autodismiss" in localStorage)
 		$("#autodismiss").val(localStorage["autodismiss"]);
 	$("#autodismiss").change(autodismiss_set);
+	if ("true" == localStorage["notify"] || false) {
+		$("#notification_settings").show();
+		$("#notify").prop('checked', true);
+	}
+	$("#notify").change(notification_toggle);
+	$("#poll_interval").val(localStorage["poll_interval"] || 15);
+
+	if ("true" == localStorage["use_ssl"] || false)
+		$("#use_ssl").prop('checked', true);
+
+	$("#use_ssl").change(function() {localStorage["use_ssl"] = $(this).is(':checked');});
+
+	if ("true" == localStorage["notify_af1"] || false)
+		$("#af1").prop('checked', true);
+	if ("true" == localStorage["notify_af2"] || false)
+		$("#af2").prop('checked', true);
+	if ("true" == localStorage["notify_af3"] || false)
+		$("#af3").prop('checked', true);
+
+	$("#af1").change(function() {localStorage["notify_af1"] = $(this).is(':checked');});
+	$("#af2").change(function() {localStorage["notify_af2"] = $(this).is(':checked');});
+	$("#af3").change(function() {localStorage["notify_af3"] = $(this).is(':checked');});
 });
